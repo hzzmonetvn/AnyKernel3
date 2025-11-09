a2d84fead36506410bfef4d554cdd2c491ab4476
Magic Cool <hzzmonet7@gmail.com>
2025-11-09 12:56:11 +0700

    No

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Kernels by TheWildJames aka Morgan Weedman
+kernel.string=KernelSU by HzzMonet✨
 do.devicecheck=0
 do.modules=1
 do.systemless=0
